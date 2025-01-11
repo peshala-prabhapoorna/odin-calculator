bcc3816a9f2fa5422528904458e0da81fcbf14c4
peshala-prabhapoorna <peshalaprabhapoorna@gmail.com>
2025-01-11 21:19:36 +0530

    feat: evaluate input
    It works nicely as intended, but the code is ugly af.

--- script.js
+++ script.js
@@ -29,3 +29,84 @@ backspaceBtn.addEventListener('click', () => {
     }
     inputSection.textContent = inputLiteral.slice(0, -1);
 });
+
+calculateBtn.addEventListener('click', () => {
+    const input = inputSection.textContent;
+    if (input === '') return;
+
+    const tokens = parse(input);
+    const result = evaluate(tokens);
+    const resultRounded = Math.round(result * 100) / 100;
+    resultSection.textContent = `= ${resultRounded}`;
+});
+
+function parse(input) {
+    let splitInput = input.split(' ');
+    // empty char is added when two spaces chars are split
+    // there are two spaces between operator and negative number
+    splitInput = splitInput.filter(item => item !== '');
+
+    const tokens = [];
+    let index = 0;
+    let isAtEnd = false;
+    const seek = () => {
+        if (index >= splitInput.length - 1) {
+            isAtEnd = true;
+            // indicate end of splitInput array
+            return false;
+        }
+        index++;
+    }
+
+    // parse the first number
+    if (isNumeric(splitInput[index])) {
+        tokens.push(+splitInput[index]);
+        seek();
+    } else if (isNumeric(splitInput[index] + splitInput[index + 1])) {
+        tokens.push(+(splitInput[index] + splitInput[index + 1]));
+        seek();
+        seek();
+    }
+    while(!isAtEnd) {
+        // a number must be followed by an operator if input isn't at end
+        tokens.push(splitInput[index]);
+        if (seek()) break;
+        // an operator must be follow by a number if input isn't at end
+        if (isNumeric(splitInput[index])) {
+            tokens.push(+splitInput[index]);
+            if (seek()) break;
+        } else if (isNumeric(splitInput[index] + splitInput[index + 1])) {
+            tokens.push(+(splitInput[index] + splitInput[index + 1]));
+            if (seek()) break;
+            if (seek()) break;
+        }
+    }
+    return tokens;
+}
+
+function isNumeric(n) {
+  return !isNaN(parseFloat(n)) && isFinite(n);
+}
+
+function evaluate(tokens) {
+    // deal with multiplication and division
+    while (tokens.includes('*') || tokens.includes('/')) {
+        const index = tokens.findIndex(item => item === '*' || item === '/');
+        if (tokens[index] === '*') {
+            tokens.splice(index - 1, 3, tokens[index - 1] * tokens[index + 1]);
+        } else {
+            tokens.splice(index - 1, 3, tokens[index - 1] / tokens[index + 1]);
+        }
+    }
+
+    // deal with addition and substraction
+    while (tokens.includes('+') || tokens.includes('-')) {
+        const index = tokens.findIndex(item => item === '+' || item === '-');
+        if (tokens[index] === '+') {
+            tokens.splice(index - 1, 3, tokens[index - 1] + tokens[index + 1]);
+        } else {
+            tokens.splice(index - 1, 3, tokens[index - 1] - tokens[index + 1]);
+        }
+    }
+    return tokens[0];
+}
